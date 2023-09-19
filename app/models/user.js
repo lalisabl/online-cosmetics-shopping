c8b2cd807bfc460ac9b76a8c801663d8753592b1
tokukiyu <tokkiyuam@gmail.com>
2023-09-19 16:42:34 +0300

    user model -password hashing

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -34,6 +36,14 @@ const userSchema = new mongoose.Schema({
     },
   ],
 });
+
+userSchema.methods.hashPassword = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
