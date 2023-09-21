5f84ce06502bb7eaa56d79fabb28d56fa9025233
tokukiyu <tokkiyuam@gmail.com>
2023-09-21 21:39:27 +0300

    passport left behind jwt chosen

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const passport = require('passport');
+const passport = require("passport");
 const userController = require("../controllers/userController");
-router
-  .route("/")
-  .get(userController.getAllUsers);
+router.route("/").get(userController.getAllUsers);
 router.post("/register", userController.createNewAccount);
-router.get('/u')
-router.post('/login', passport.authenticate('local', { failureRedirect: '/login-failure' }), (req, res) => {
-  // Successful login
-  const token = generateJwtToken(req.user); // Generate a JWT token
-  res.json({ token }); // Return the token to the client
-});
+router.get("/u");
+router.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login-failure" }),
+  (req, res) => {
+    // Successful login
+    const token = generateJwtToken(req.user);
+    res.json({ token });
+  }
+);
 
 module.exports = router;
