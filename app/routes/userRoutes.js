341352fd5e223cd397bbb39cea50e6dc8ef220dc
tokukiyu <tokkiyuam@gmail.com>
2023-09-21 21:57:29 +0300

    login update

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -6,24 +6,7 @@ const secretKey = process.env.SECRET_KEY;
 const userController = require("../controllers/userController");
 router.route("/").get(userController.getAllUsers);
 router.post("/register", userController.createNewAccount);
-//user 
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (!user || !user.validatePassword(password)) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-    const token = jwt.sign(
-      { userId: user._id, email: user.email, role: user.role },
-      secretKey,
-      { expiresIn: "1h" }
-    );
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+//user
+router.post("/login", userController.loginUsers);
 
 module.exports = router;
