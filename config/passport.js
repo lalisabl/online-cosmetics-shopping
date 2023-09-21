d0af17661af1cac4aebf034b8a45e80a52aa2fed
tokukiyu <tokkiyuam@gmail.com>
2023-09-21 15:05:00 +0300

    passport configuring starter

---
+++ config/passport.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/User'); // Import your User model
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      if (!user.validatePassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
