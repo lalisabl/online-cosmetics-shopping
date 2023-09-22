662a5b734fbba2c0f8d5eeca3c410847db0dc719
tokukiyu <tokkiyuam@gmail.com>
2023-09-22 12:07:09 +0300

    clean and cors usage

--- app/app.js
+++ app/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const dbConn = require("../config/db");
+
+const cors = require('cors'); // Import the cors middleware
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 const productRoute = require("./routes/productRoutes");
 const userRoute = require("./routes/userRoutes");
 const orderRoute = require("./routes/orderRoutes");
