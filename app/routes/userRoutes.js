6b735883a5b5024210c5814d7c78023adc113270
tokukiyu <tokkiyuam@gmail.com>
2023-09-24 10:09:17 +0300

    u

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -6,5 +6,5 @@ router.route("/").get(userController.getAllUsers);
 router.post("/register", userController.createNewAccount);
 //user
 router.post("/login", userController.loginUsers);
-router.get("/myprofile", middleWare.authenticateJWT, userController.myProfile);
+router.post("/myprofile", middleWare.authenticateJWT, userController.myProfile);
 module.exports = router;
