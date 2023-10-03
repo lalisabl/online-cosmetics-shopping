2419f37535b7160f48526241ce01d78a081fe8b7
lbula <bulalalisa@gmail.com>
2023-10-03 14:24:07 -0400

    cart model is added

--- app/models/cart.js
+++ app/models/cart.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const cartItemSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product', 
+    ref: "Product",
     required: true,
   },
   quantity: {
     type: Number,
     required: true,
-    min: 1, 
+    min: 1,
   },
 });
 
 const cartSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', 
+    ref: "User",
     required: true,
   },
   items: [cartItemSchema],
 });
 
-const Cart = mongoose.model('Cart', cartSchema);
+const Cart = mongoose.model("Cart", cartSchema);
 
 module.exports = Cart;
