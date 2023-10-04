552745a6fb2ce216e59d7d952c99dffca2722da1
lbula <bulalalisa@gmail.com>
2023-10-04 08:28:28 -0400

    error correction to the existing code

--- app/routes/cartRoutes.js
+++ app/routes/cartRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
-router.post("/add", cartController.addToCart);
+router.post("/addProducts", cartController.addToCart);
+router.route("/").get(cartController.viewCart);
 module.exports = router;
