ed09e5687560065d92939fbfac11776195ccd914
Tsion Ephrem <tsionephrem7@gmail.com>
2023-10-16 19:42:17 +0300

    made the product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Card from "./Components/Card/Card";
+import ProductList from "./Components/Product/ProductList";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductList />
     </div>
   );
 }
