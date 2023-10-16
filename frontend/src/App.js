cd5e061201d35942f17d65d306e4cc67103af458
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-10-16 22:50:21 +0300

    card  and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Card from "./Components/Card/Card";
-import ProductList from "./Components/Product/ProductList";
 
 function App() {
   return (
     <div className="App">
       <Header />
-<<<<<<< HEAD
-      <ProductList />
-=======
       <Card />
->>>>>>> 1b3dd8b (Addin Card and Header)
     </div>
   );
 }
