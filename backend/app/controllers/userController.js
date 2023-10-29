91ed4543d7de424c3173f6561da577a170a001b0
lbula <bulalalisa@gmail.com>
2023-10-29 18:47:42 -0400

    multi searched added to products

--- backend/app/controllers/userController.js
+++ backend/app/controllers/userController.js
@@ -103,3 +103,20 @@ exports.deleteUser = async (req, res, next) => {
     next(err);
   }
 };
+exports.updateOne = catchAsync(async (req, res, next) => {
+  const doc = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!doc) {
+    return next(new AppError("No document found with that ID", 404));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: doc,
+    },
+  });
+});
